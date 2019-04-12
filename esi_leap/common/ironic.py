9e6acbd822792d58bf380b7e1dfe8b74b0b25832
Tzu-Mainn Chen <tzumainn@redhat.com>
2019-04-12 17:22:54 +0000

    created ironic client

---
+++ esi_leap/common/ironic.py
@@ -0,0 +1,36 @@
+from keystoneauth1 import discover as ks_disc
+from keystoneauth1 import loading as ks_loading
+
+from ironicclient import client as ironic_client
+
+from esi_leap.common import exception
+import esi_leap.conf
+
+
+CONF = esi_leap.conf.CONF
+_cached_ironic_client = None
+
+def get_ironic_client():
+    global _cached_ironic_client
+    if _cached_ironic_client is not None:
+        return _cached_ironic_client
+    
+    auth_plugin = ks_loading.load_auth_from_conf_options(CONF, 'ironic')
+    sess = ks_loading.load_session_from_conf_options(CONF, 'ironic',
+                                                     auth=auth_plugin)
+
+    try:
+        ksa_adap = utils.get_ksa_adapter(
+            'baremetal',
+            ksa_auth=auth_plugin, ksa_session=sess)
+        ironic_url = ksa_adap.get_endpoint()
+    except:
+        ironic_url = None
+
+    kwargs = {}
+    cli = ironic_client.get_client(1,
+                                   endpoint=ironic_url,
+                                   session=sess, **kwargs)
+    _cached_ironic_client = cli
+
+    return cli
