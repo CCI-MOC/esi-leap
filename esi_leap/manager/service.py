7c83264525d8af62ac21f0c02fec55d38b7472c3
Tzu-Mainn Chen <tzumainn@redhat.com>
2019-05-01 18:42:43 +0000

    add periodic jobs to monitor lease requests and nodes

--- esi_leap/manager/service.py
+++ esi_leap/manager/service.py
@@ -14,10 +14,15 @@ from oslo_context import context as ctx
 from oslo_log import log as logging
 import oslo_messaging as messaging
 from oslo_service import service
+from oslo_utils import timeutils
 
+from esi_leap.common import statuses
 import esi_leap.conf
 from esi_leap.db import api as db_api
 from esi_leap.manager import utils
+from esi_leap.objects import lease_request
+from esi_leap.objects import policy_node
+
 
 CONF = esi_leap.conf.CONF
 EVENT_INTERVAL = 10
@@ -27,25 +32,82 @@ LOG = logging.getLogger(__name__)
 class ManagerService(service.Service):
     def __init__(self):
         super(ManagerService, self).__init__()
-        LOG.debug("Creating esi-leap manager RPC server")
+        LOG.info("Creating esi-leap manager RPC server")
         self._server = messaging.get_rpc_server(
             target=utils.get_target(),
             transport=messaging.get_rpc_transport(CONF),
             endpoints=[ManagerEndpoint()],
             executor='eventlet',
         )
+        self._context = ctx.RequestContext(
+            auth_token=None,
+            project_id=None,
+            is_admin=True,
+            overwrite=False)
 
     def start(self):
         super(ManagerService, self).start()
-        LOG.debug("Starting esi-leap manager RPC server")
+        LOG.info("Starting esi-leap manager RPC server")
         self.tg.add_thread(self._server.start)
-        # TODO: this will need to be implemented, this is a reminder as to how
-        # self.tg.add_timer(EVENT_INTERVAL, self._lease_request_something)
+        LOG.info("Starting _fulfill_leases periodic job")
+        self.tg.add_timer(300, self._fulfill_leases)
+        LOG.info("Starting _expire_or_cancel_leases periodic job")
+        self.tg.add_timer(60, self._expire_or_cancel_leases)
+        LOG.info("Starting _expire_policy_node_leases periodic job")
+        self.tg.add_timer(60, self._expire_policy_node_leases)
+        LOG.info("Starting _expire_policy_nodes periodic job")
+        self.tg.add_timer(60, self._expire_policy_nodes)
 
     def stop(self):
         super(ManagerService, self).stop()
-        LOG.debug("Shutting down esi-leap manager RPC server")
+        LOG.info("Shutting down esi-leap manager RPC server")
         self._server.stop()
+
+    def _fulfill_leases(self):
+        LOG.info("Running _fulfill_leases")
+        leases = lease_request.LeaseRequest.get_all_by_status(
+            self._context, statuses.PENDING)
+        for lease in leases:
+            LOG.info("Trying to fulfill lease %s", lease.uuid)
+            lease.fulfill(self._context)
+
+    def _expire_or_cancel_leases(self):
+        LOG.info("Running _expire_or_cancel_leases")
+        pending_leases = lease_request.LeaseRequest.get_all_by_status(
+            self._context, statuses.PENDING)
+        for lease in pending_leases:
+            if lease.cancel_date and lease.cancel_date <= timeutils.utcnow():
+                LOG.info("Cancelling lease %s", lease.uuid)
+                lease.expire_or_cancel(self._context, statuses.CANCELLED)
+
+        fulfilled_leases = lease_request.LeaseRequest.get_all_by_status(
+            self._context, statuses.FULFILLED)
+        expired_leases = lease_request.LeaseRequest.get_all_by_status(
+            self._context, statuses.DEGRADED)
+        for lease in fulfilled_leases + expired_leases:
+            if lease.expiration_date and \
+               lease.expiration_date <= timeutils.utcnow():
+                LOG.info("Expiring lease %s", lease.uuid)
+                lease.expire_or_cancel(self._context)
+
+    def _expire_policy_node_leases(self):
+        LOG.info("Running _expire_policy_node_leases")
+        nodes = policy_node.PolicyNode.get_leased(self._context)
+        for node in nodes:
+            if node.lease_expiration_date and \
+               node.lease_expiration_date <= timeutils.utcnow():
+                LOG.info("Unassigning node %s from lease %s",
+                         node.node_uuid, node.request_uuid)
+                node.unassign_node(self._context)
+
+    def _expire_policy_nodes(self):
+        LOG.info("Running _expire_policy_nodes")
+        nodes = policy_node.PolicyNode.get_all(self._context)
+        for node in nodes:
+            if node.expiration_date and \
+               node.expiration_date <= timeutils.utcnow():
+                LOG.info("Expiring node %s", node.node_uuid)
+                node.destroy(self._context)
 
 
 class ManagerEndpoint(object):
