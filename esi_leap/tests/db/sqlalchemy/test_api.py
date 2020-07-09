80cb8557f1a0467c9d85f4b6bba21724ec8a92a5
cudkowicz <dcudkowicz@gmail.com>
2020-07-09 18:46:53 +0000

    Tests for api.py
    Added tests to api for all methods and possibilites of all methods for Contracts and Offers.

--- esi_leap/tests/db/sqlalchemy/test_api.py
+++ esi_leap/tests/db/sqlalchemy/test_api.py
@@ -100,7 +100,16 @@ test_contract_4 = dict(
     name='c2',
     start_time=now + datetime.timedelta(days=85),
     end_time=now + datetime.timedelta(days=90),
+    properties={},
     status=statuses.CANCELLED,
+)
+
+test_contract_5 = dict(
+    project_id='1e5533',
+    start_time=now + datetime.timedelta(days=90),
+    end_time=now + datetime.timedelta(days=100),
+    uuid='contract_5',
+    properties={},
 )
 
 
@@ -226,21 +235,16 @@ class TestAPI(base.DBTestCase):
         api.offer_verify_contract_availability(offer, start, end)
 
     def test_offer_get_by_uuid(self):
-
         offer = api.offer_create(test_offer_1)
-        api.offer_create(test_offer_2)
-
         res = api.offer_get_by_uuid(offer.uuid)
         self.assertEqual(offer.uuid, res.uuid)
         self.assertEqual(offer.project_id, res.project_id)
         self.assertEqual(offer.properties, res.properties)
 
     def test_offer_get_by_uuid_not_found(self):
-
         assert api.offer_get_by_uuid('some_uuid') is None
 
     def test_offer_get_by_name(self):
-
         o1 = api.offer_create(test_offer_1)
         o2 = api.offer_create(test_offer_2)
         o3 = api.offer_create(test_offer_3)
@@ -258,5 +262,104 @@ class TestAPI(base.DBTestCase):
         self.assertEqual(o3.project_id, res[2].project_id)
 
     def test_offer_get_by_name_not_found(self):
+        self.assertEqual(api.offer_get_by_name('some_name'), [])
 
-        assert api.offer_get_by_uuid('some_uuid') is None
+    def test_offer_destroy(self):
+        offer = api.offer_create(test_offer_2)
+        api.offer_destroy(offer.uuid)
+        self.assertEqual(api.offer_get_by_uuid('offer_2'), None)
+
+    def test_offer_destroy_not_found(self):
+        self.assertEqual(api.offer_get_by_uuid('offer_4'), None)
+
+    def test_offer_update(self):
+        o1 = api.offer_create(test_offer_3)
+        values = {'start_time': test_offer_2['start_time'],
+                  'end_time': test_offer_2['end_time']}
+        api.offer_update(o1.uuid, values)
+        self.assertEqual(test_offer_2['start_time'], o1.start_time)
+        self.assertEqual(test_offer_2['end_time'], o1.end_time)
+
+    def test_offer_get_all(self):
+        o1 = api.offer_create(test_offer_2)
+        o2 = api.offer_create(test_offer_3)
+        res = api.offer_get_all({})
+        self.assertEqual((o1.to_dict(), o2.to_dict()),
+                         (res[0].to_dict(), res[1].to_dict()))
+
+    def test_contract_get_by_uuid(self):
+        o = api.offer_create(test_offer_2)
+        test_contract_4['offer_uuid'] = o.uuid
+        contract = api.contract_create(test_contract_4)
+        res = api.contract_get_by_uuid(contract.uuid)
+        self.assertEqual(contract.uuid, res.uuid)
+
+    def test_contract_get_by_uuid_not_found(self):
+        assert api.contract_get_by_uuid('some_uuid') is None
+
+    def test_contract_get_by_name(self):
+        o1 = api.offer_create(test_offer_1)
+        o2 = api.offer_create(test_offer_2)
+        o3 = api.offer_create(test_offer_3)
+        o4 = api.offer_create(test_offer_4)
+        test_contract_1['offer_uuid'] = o1.uuid
+        test_contract_2['offer_uuid'] = o2.uuid
+        test_contract_3['offer_uuid'] = o3.uuid
+        test_contract_4['offer_uuid'] = o4.uuid
+        c1 = api.contract_create(test_contract_1)
+        c2 = api.contract_create(test_contract_2)
+        c3 = api.contract_create(test_contract_3)
+        api.contract_create(test_contract_4)
+        res = api.contract_get_by_name('c1')
+        assert len(res) == 3
+        self.assertEqual(c1.uuid, res[0].uuid)
+        self.assertEqual(c1.project_id, res[0].project_id)
+
+        self.assertEqual(c2.uuid, res[1].uuid)
+        self.assertEqual(c2.project_id, res[1].project_id)
+
+        self.assertEqual(c3.uuid, res[2].uuid)
+        self.assertEqual(c3.project_id, res[2].project_id)
+
+    def test_contract_get_by_name_not_found(self):
+        self.assertEqual(api.contract_get_by_name('some_name'), [])
+
+    def test_contract_get_all(self):
+        o1 = api.offer_create(test_offer_2)
+        test_contract_4['offer_uuid'] = o1.uuid
+        o2 = api.offer_create(test_offer_3)
+        test_contract_5['offer_uuid'] = o2.uuid
+        c1 = api.contract_create(test_contract_4)
+        c2 = api.contract_create(test_contract_5)
+        res = api.contract_get_all({})
+        self.assertEqual((c1.to_dict(), c2.to_dict()),
+                         (res[0].to_dict(), res[1].to_dict()))
+
+    def test_contract_create(db):
+        o = api.offer_create(test_offer_2)
+        test_contract_4['offer_uuid'] = o.uuid
+        c1 = api.contract_create(test_contract_4)
+        c2 = api.contract_get_all({}).all()
+        assert len(c2) == 1
+        assert c2[0].to_dict() == c1.to_dict()
+
+    def test_contract_update(self):
+        o1 = api.offer_create(test_offer_2)
+        test_contract_4['offer_uuid'] = o1.uuid
+        c1 = api.contract_create(test_contract_4)
+        values = {'start_time': test_contract_5['start_time'],
+                  'end_time': test_contract_5['end_time']}
+        api.contract_update(c1.uuid, values)
+        c1 = api.contract_get_by_uuid(c1.uuid)
+        self.assertEqual(test_contract_5['start_time'], c1.start_time)
+        self.assertEqual(test_contract_5['end_time'], c1.end_time)
+
+    def test_contract_destroy(self):
+        o = api.offer_create(test_offer_2)
+        test_contract_4['offer_uuid'] = o.uuid
+        contract = api.contract_create(test_contract_4)
+        api.contract_destroy(contract.uuid)
+        self.assertEqual(api.contract_get_by_uuid('contract_4'), None)
+
+    def test_contract_destroy_not_found(self):
+        self.assertEqual(api.contract_get_by_uuid('contract_4'), None)
