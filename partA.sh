ee9690306ea1da84bf15a738462bda72a1d844b0
cudkowicz <dcudkowicz@gmail.com>
2020-08-05 15:32:37 +0000

    Testing for speed (WIP)
    A test for how quickly commands operate in the database.

---
+++ partA.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+mkdir testDir
+cd testDir
+echo "test" > file1.txt
+echo "test" > file2.txt
+echo "test" > file3.txt
---
+++ partC.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+cd testDir
+rm file1.txt
+rm file2.txt
+rm file3.txt
