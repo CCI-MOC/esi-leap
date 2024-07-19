larsks/workflow-test

Rename TestNode to FakeNode
Classes that start with `Test` are identified as test cases, causing
warnings during test discovery. In this commit we rename `TestNode` to
`FakeNode` to prevent it from being erroneously collected as a test.

esi_leap/tests/api/controllers/v1/test_event.py
@@ -14,7 +14,7 @@ from datetime import datetime
 import mock
 
 from esi_leap.common import exception
-from esi_leap.resource_objects.test_node import TestNode
+from esi_leap.resource_objects.test_node import FakeNode
 from esi_leap.tests.api import base as test_api_base
 
 
@@ -82,7 +82,7 @@ class TestEventsController(test_api_base.APITestCase):
         fake_event = FakeEvent()
         expected_filters = {"resource_type": "test_node", "resource_uuid": "1111"}
         mock_pa.side_effect = None
-        mock_gro.return_value = TestNode("1111")
+        mock_gro.return_value = FakeNode("1111")
         mock_ega.return_value = [fake_event]
 
         data = self.get_json("/events?resource_uuid=1111&resource_type=test_node")

esi_leap/tests/api/controllers/v1/test_lease.py
@@ -23,7 +23,7 @@ from esi_leap.common import exception
 from esi_leap.common import statuses
 from esi_leap.objects import lease as lease_obj
 from esi_leap.resource_objects.ironic_node import IronicNode
-from esi_leap.resource_objects.test_node import TestNode
+from esi_leap.resource_objects.test_node import FakeNode
 from esi_leap.tests.api import base as test_api_base
 
 
@@ -98,7 +98,7 @@ class TestLeasesController(test_api_base.APITestCase):
         mock_gro,
         mock_lgdwai,
     ):
-        resource = TestNode("1234567890")
+        resource = FakeNode("1234567890")
         data = {
             "project_id": "lesseeid",
             "resource_type": "test_node",
@@ -453,7 +453,7 @@ class TestLeasesController(test_api_base.APITestCase):
     def test_get_resource_filter(
         self, mock_get_all, mock_lgaaf, mock_gro, mock_lgdwai, mock_gpl, mock_gnl
     ):
-        mock_gro.return_value = TestNode("54321")
+        mock_gro.return_value = FakeNode("54321")
         mock_get_all.return_value = [self.test_lease, self.test_lease]
         mock_gpl.return_value = []
         mock_gnl.return_value = []

esi_leap/tests/api/controllers/v1/test_offer.py
@@ -19,7 +19,7 @@ from esi_leap.common import exception
 from esi_leap.common import statuses
 from esi_leap.objects import offer
 from esi_leap.resource_objects.ironic_node import IronicNode
-from esi_leap.resource_objects.test_node import TestNode
+from esi_leap.resource_objects.test_node import FakeNode
 from esi_leap.tests.api import base as test_api_base
 
 
@@ -124,7 +124,7 @@ class TestOffersController(test_api_base.APITestCase):
     def test_post(
         self, mock_ogdwai, mock_create, mock_cra, mock_generate_uuid, mock_gro
     ):
-        resource = TestNode(self.test_offer.resource_uuid)
+        resource = FakeNode(self.test_offer.resource_uuid)
         mock_gro.return_value = resource
         mock_generate_uuid.return_value = self.test_offer.uuid
         mock_create.return_value = self.test_offer
@@ -210,7 +210,7 @@ class TestOffersController(test_api_base.APITestCase):
         mock_gpufi,
         mock_gro,
     ):
-        resource = TestNode(self.test_offer_lessee.resource_uuid)
+        resource = FakeNode(self.test_offer_lessee.resource_uuid)
         mock_gro.return_value = resource
         mock_gpufi.return_value = "lessee_uuid"
         mock_generate_uuid.return_value = self.test_offer_lessee.uuid
@@ -260,7 +260,7 @@ class TestOffersController(test_api_base.APITestCase):
         mock_gro,
         mock_crla,
     ):
-        resource = TestNode(self.test_offer_with_parent.resource_uuid)
+        resource = FakeNode(self.test_offer_with_parent.resource_uuid)
         mock_gro.return_value = resource
         mock_generate_uuid.return_value = self.test_offer_with_parent.uuid
         mock_create.return_value = self.test_offer_with_parent
@@ -320,7 +320,7 @@ class TestOffersController(test_api_base.APITestCase):
         mock_gro,
         mock_crla,
     ):
-        resource = TestNode(self.test_offer_with_parent.resource_uuid)
+        resource = FakeNode(self.test_offer_with_parent.resource_uuid)
         mock_gro.return_value = resource
         mock_generate_uuid.return_value = self.test_offer_with_parent.uuid
         mock_create.return_value = self.test_offer_with_parent
@@ -496,7 +496,7 @@ class TestOffersController(test_api_base.APITestCase):
             _get_offer_response(self.test_offer, use_datetime=True),
             _get_offer_response(self.test_offer_2, use_datetime=True),
         ]
-        mock_gro.return_value = TestNode("54321")
+        mock_gro.return_value = FakeNode("54321")
         mock_gpl.return_value = []
         mock_gnl.return_value = []
 

esi_leap/tests/api/controllers/v1/test_utils.py
@@ -24,7 +24,7 @@ from esi_leap.common import policy
 from esi_leap.common import statuses
 from esi_leap.objects import lease
 from esi_leap.objects import offer
-from esi_leap.resource_objects.test_node import TestNode
+from esi_leap.resource_objects.test_node import FakeNode
 
 
 admin_ctx = ctx.RequestContext(project_id="adminid", roles=["admin"])
@@ -45,8 +45,8 @@ start_iso = "2016-07-16T00:00:00"
 end = start + datetime.timedelta(days=100)
 end_iso = "2016-10-24T00:00:00"
 
-test_node_1 = TestNode("111", owner_ctx.project_id)
-test_node_2 = TestNode("bbb", owner_ctx_2.project_id)
+test_node_1 = FakeNode("111", owner_ctx.project_id)
+test_node_2 = FakeNode("bbb", owner_ctx_2.project_id)
 
 o_uuid = uuidutils.generate_uuid()
 
@@ -684,11 +684,11 @@ class TestLeaseGetDictWithAddedInfoUtils(testtools.TestCase):
             parent_lease_uuid=None,
         )
 
-    @mock.patch("esi_leap.resource_objects.test_node.TestNode.get_name")
+    @mock.patch("esi_leap.resource_objects.test_node.FakeNode.get_name")
     @mock.patch("esi_leap.common.keystone.get_project_name")
     @mock.patch("esi_leap.objects.lease.get_resource_object")
     def test_lease_get_dict_with_added_info(self, mock_gro, mock_gpn, mock_gn):
-        mock_gro.return_value = TestNode("111")
+        mock_gro.return_value = FakeNode("111")
         mock_gpn.return_value = "project-name"
         mock_gn.return_value = "resource-name"
 

esi_leap/tests/common/test_notification_utils.py
@@ -18,7 +18,7 @@ from oslo_utils import uuidutils
 from esi_leap.common import notification_utils as notif_utils
 from esi_leap.objects import fields
 from esi_leap.objects import lease as lease_obj
-from esi_leap.resource_objects.test_node import TestNode
+from esi_leap.resource_objects.test_node import FakeNode
 from esi_leap.tests import base as tests_base
 
 
@@ -48,7 +48,7 @@ class NotifyTestCase(tests_base.TestCase):
             status="created",
             purpose=None,
         )
-        self.node = TestNode(uuidutils.generate_uuid(), "12345")
+        self.node = FakeNode(uuidutils.generate_uuid(), "12345")
 
     def test_emit_notification(self):
         self.config(host="fake-host")

esi_leap/tests/resource_objects/test_resource_objects.py
@@ -22,7 +22,7 @@ class TestResourceObjects(base.TestCase):
         types = {
             "ironic_node": resource_objects.ironic_node.IronicNode,
             "dummy_node": resource_objects.dummy_node.DummyNode,
-            "test_node": resource_objects.test_node.TestNode,
+            "test_node": resource_objects.test_node.FakeNode,
         }
 
         for type_name, expected_type in types.items():
@@ -84,7 +84,7 @@ class TestResourceObjects(base.TestCase):
     def test_test_node(self):
         node = resource_objects.get_resource_object("test_node", "1111")
 
-        self.assertIsInstance(node, resource_objects.test_node.TestNode)
+        self.assertIsInstance(node, resource_objects.test_node.FakeNode)
         self.assertEqual("1111", node.get_uuid())
 
     def test_unknown_resource_type(self):

CCI-MOC/esi-leap

esi_leap/tests/api/controllers/v1/test_event.py
@@ -14,7 +14,7 @@ from datetime import datetime
 import mock
 
 from esi_leap.common import exception
-from esi_leap.resource_objects.test_node import TestNode
+from esi_leap.resource_objects.fake_node import FakeNode
 from esi_leap.tests.api import base as test_api_base
 
 
@@ -82,7 +82,7 @@ class TestEventsController(test_api_base.APITestCase):
         fake_event = FakeEvent()
         expected_filters = {"resource_type": "test_node", "resource_uuid": "1111"}
         mock_pa.side_effect = None
-        mock_gro.return_value = TestNode("1111")
+        mock_gro.return_value = FakeNode("1111")
         mock_ega.return_value = [fake_event]
 
         data = self.get_json("/events?resource_uuid=1111&resource_type=test_node")

esi_leap/tests/api/controllers/v1/test_lease.py
@@ -23,7 +23,7 @@ from esi_leap.common import exception
 from esi_leap.common import statuses
 from esi_leap.objects import lease as lease_obj
 from esi_leap.resource_objects.ironic_node import IronicNode
-from esi_leap.resource_objects.test_node import TestNode
+from esi_leap.resource_objects.fake_node import FakeNode
 from esi_leap.tests.api import base as test_api_base
 
 
@@ -98,7 +98,7 @@ class TestLeasesController(test_api_base.APITestCase):
         mock_gro,
         mock_lgdwai,
     ):
-        resource = TestNode("1234567890")
+        resource = FakeNode("1234567890")
         data = {
             "project_id": "lesseeid",
             "resource_type": "test_node",
@@ -453,7 +453,7 @@ class TestLeasesController(test_api_base.APITestCase):
     def test_get_resource_filter(
         self, mock_get_all, mock_lgaaf, mock_gro, mock_lgdwai, mock_gpl, mock_gnl
     ):
-        mock_gro.return_value = TestNode("54321")
+        mock_gro.return_value = FakeNode("54321")
         mock_get_all.return_value = [self.test_lease, self.test_lease]
         mock_gpl.return_value = []
         mock_gnl.return_value = []

esi_leap/tests/api/controllers/v1/test_offer.py
@@ -19,7 +19,7 @@ from esi_leap.common import exception
 from esi_leap.common import statuses
 from esi_leap.objects import offer
 from esi_leap.resource_objects.ironic_node import IronicNode
-from esi_leap.resource_objects.test_node import TestNode
+from esi_leap.resource_objects.fake_node import FakeNode
 from esi_leap.tests.api import base as test_api_base
 
 
@@ -124,7 +124,7 @@ class TestOffersController(test_api_base.APITestCase):
     def test_post(
         self, mock_ogdwai, mock_create, mock_cra, mock_generate_uuid, mock_gro
     ):
-        resource = TestNode(self.test_offer.resource_uuid)
+        resource = FakeNode(self.test_offer.resource_uuid)
         mock_gro.return_value = resource
         mock_generate_uuid.return_value = self.test_offer.uuid
         mock_create.return_value = self.test_offer
@@ -210,7 +210,7 @@ class TestOffersController(test_api_base.APITestCase):
         mock_gpufi,
         mock_gro,
     ):
-        resource = TestNode(self.test_offer_lessee.resource_uuid)
+        resource = FakeNode(self.test_offer_lessee.resource_uuid)
         mock_gro.return_value = resource
         mock_gpufi.return_value = "lessee_uuid"
         mock_generate_uuid.return_value = self.test_offer_lessee.uuid
@@ -260,7 +260,7 @@ class TestOffersController(test_api_base.APITestCase):
         mock_gro,
         mock_crla,
     ):
-        resource = TestNode(self.test_offer_with_parent.resource_uuid)
+        resource = FakeNode(self.test_offer_with_parent.resource_uuid)
         mock_gro.return_value = resource
         mock_generate_uuid.return_value = self.test_offer_with_parent.uuid
         mock_create.return_value = self.test_offer_with_parent
@@ -320,7 +320,7 @@ class TestOffersController(test_api_base.APITestCase):
         mock_gro,
         mock_crla,
     ):
-        resource = TestNode(self.test_offer_with_parent.resource_uuid)
+        resource = FakeNode(self.test_offer_with_parent.resource_uuid)
         mock_gro.return_value = resource
         mock_generate_uuid.return_value = self.test_offer_with_parent.uuid
         mock_create.return_value = self.test_offer_with_parent
@@ -496,7 +496,7 @@ class TestOffersController(test_api_base.APITestCase):
             _get_offer_response(self.test_offer, use_datetime=True),
             _get_offer_response(self.test_offer_2, use_datetime=True),
         ]
-        mock_gro.return_value = TestNode("54321")
+        mock_gro.return_value = FakeNode("54321")
         mock_gpl.return_value = []
         mock_gnl.return_value = []
 

esi_leap/tests/api/controllers/v1/test_utils.py
@@ -24,7 +24,7 @@ from esi_leap.common import policy
 from esi_leap.common import statuses
 from esi_leap.objects import lease
 from esi_leap.objects import offer
-from esi_leap.resource_objects.test_node import TestNode
+from esi_leap.resource_objects.fake_node import FakeNode
 
 
 admin_ctx = ctx.RequestContext(project_id="adminid", roles=["admin"])
@@ -45,8 +45,8 @@ start_iso = "2016-07-16T00:00:00"
 end = start + datetime.timedelta(days=100)
 end_iso = "2016-10-24T00:00:00"
 
-test_node_1 = TestNode("111", owner_ctx.project_id)
-test_node_2 = TestNode("bbb", owner_ctx_2.project_id)
+test_node_1 = FakeNode("111", owner_ctx.project_id)
+test_node_2 = FakeNode("bbb", owner_ctx_2.project_id)
 
 o_uuid = uuidutils.generate_uuid()
 
@@ -684,11 +684,11 @@ class TestLeaseGetDictWithAddedInfoUtils(testtools.TestCase):
             parent_lease_uuid=None,
         )
 
-    @mock.patch("esi_leap.resource_objects.test_node.TestNode.get_name")
+    @mock.patch("esi_leap.resource_objects.fake_node.FakeNode.get_name")
     @mock.patch("esi_leap.common.keystone.get_project_name")
     @mock.patch("esi_leap.objects.lease.get_resource_object")
     def test_lease_get_dict_with_added_info(self, mock_gro, mock_gpn, mock_gn):
-        mock_gro.return_value = TestNode("111")
+        mock_gro.return_value = FakeNode("111")
         mock_gpn.return_value = "project-name"
         mock_gn.return_value = "resource-name"
 

esi_leap/tests/common/test_notification_utils.py
@@ -18,7 +18,7 @@ from oslo_utils import uuidutils
 from esi_leap.common import notification_utils as notif_utils
 from esi_leap.objects import fields
 from esi_leap.objects import lease as lease_obj
-from esi_leap.resource_objects.test_node import TestNode
+from esi_leap.resource_objects.fake_node import FakeNode
 from esi_leap.tests import base as tests_base
 
 
@@ -48,7 +48,7 @@ class NotifyTestCase(tests_base.TestCase):
             status="created",
             purpose=None,
         )
-        self.node = TestNode(uuidutils.generate_uuid(), "12345")
+        self.node = FakeNode(uuidutils.generate_uuid(), "12345")
 
     def test_emit_notification(self):
         self.config(host="fake-host")

esi_leap/tests/resource_objects/test_resource_objects.py
@@ -22,7 +22,7 @@ class TestResourceObjects(base.TestCase):
         types = {
             "ironic_node": resource_objects.ironic_node.IronicNode,
             "dummy_node": resource_objects.dummy_node.DummyNode,
-            "test_node": resource_objects.test_node.TestNode,
+            "test_node": resource_objects.fake_node.FakeNode,
         }
 
         for type_name, expected_type in types.items():
@@ -84,7 +84,7 @@ class TestResourceObjects(base.TestCase):
     def test_test_node(self):
         node = resource_objects.get_resource_object("test_node", "1111")
 
-        self.assertIsInstance(node, resource_objects.test_node.TestNode)
+        self.assertIsInstance(node, resource_objects.fake_node.FakeNode)
         self.assertEqual("1111", node.get_uuid())
 
     def test_unknown_resource_type(self):